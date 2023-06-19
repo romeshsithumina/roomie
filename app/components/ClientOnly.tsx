ef33672788fb0e407e61f2d9d647a098b563646e
Romesh Senarathna <romeshsithumina@gmail.com>
2023-06-20 00:15:47 +0530

    Started implementing Modal

---
+++ app/components/ClientOnly.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+// Using to fix a error happens when the user interact with
+// the webpage while its refreshing
+import { useEffect, useState } from "react";
+
+interface ClientOnlyProps {
+  children: React.ReactNode;
+}
+
+const ClientOnly: React.FC<ClientOnlyProps> = ({ children }) => {
+  const [hasMounted, setHasMounted] = useState(false);
+
+  useEffect(() => {
+    setHasMounted(true);
+  }, []);
+
+  if (!hasMounted) {
+    return null;
+  }
+
+  return <>{children}</>;
+};
+
+export default ClientOnly;
