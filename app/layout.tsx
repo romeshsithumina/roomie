f958d3f31c3f37978f16ebcff2535712d87ea961
Romesh Senarathna <romeshsithumina@gmail.com>
2023-06-20 23:24:04 +0530

    Day 03: Completed Modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
-          <Modal isOpen />
+          <Modal actionLabel="Submit" title="Login" isOpen />
           <Navbar />
         </ClientOnly>
         {children}
