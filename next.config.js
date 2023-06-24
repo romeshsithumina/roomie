5cb769aae8afe283c24f1a998f63a8c5ab340765
Romesh Senarathna <romeshsithumina@gmail.com>
2023-06-24 20:24:44 +0530

    Day 07: Added and fixed Facebook register and login functionality

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["lh3.googleusercontent.com"],
+    domains: [
+      "lh3.googleusercontent.com",
+      "scontent.fcmb1-2.fna.fbcdn.net",
+      "platform-lookaside.fbsbx.com",
+    ],
   },
 };
 
