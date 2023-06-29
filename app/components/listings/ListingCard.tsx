64db8205d6afbde27029a464e346958c2cc770c6
Romesh Senarathna <romeshsithumina@gmail.com>
2023-06-29 22:28:32 +0530

    Day 12: Implemented the ListingCard

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -9,6 +9,8 @@ import useCountries from "@/app/hooks/useCountries";
 import { useCallback, useMemo } from "react";
 import { format } from "date-fns";
 import Image from "next/image";
+import HeartButton from "../HeartButton";
+import Button from "../Button";
 
 interface ListingCardProps {
   data: Listing;
@@ -105,7 +107,28 @@ const ListingCard: React.FC<ListingCardProps> = ({
               transition
             "
           />
+          <div className="absolute top-3 right-3">
+            <HeartButton listingId={data.id} currentUser={currentUser} />
+          </div>
         </div>
+        <div className="font-semibold text-lg">
+          {location?.region}, {location?.label}
+        </div>
+        <div className="font-light text-neutral-500">
+          {reservationDate || data.category}
+        </div>
+        <div className="flex flex-row items-center gap-1">
+          <div className="font-semibold">$ {price}</div>
+          {!reservation && <div className="font-light">night</div>}
+        </div>
+        {onAction && actionLabel && (
+          <Button
+            disabled={disabled}
+            small
+            label={actionLabel}
+            onClick={handleCancel}
+          />
+        )}
       </div>
     </div>
   );
