69ad4cb591efdfead898a35c188de482998a7ec7
Romesh Senarathna <romeshsithumina@gmail.com>
2023-07-08 01:37:16 +0530

    Completed My properties

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -120,7 +120,10 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
                   onClick={() => router.push("/reservations")}
                   label="My reservations"
                 />
-                <MenuItem onClick={() => {}} label="My properties" />
+                <MenuItem
+                  onClick={() => router.push("/properties")}
+                  label="My properties"
+                />
                 <MenuItem onClick={rentModal.onOpen} label="Add my property" />
                 <hr />
                 <MenuItem onClick={() => signOut()} label="Logout" />
