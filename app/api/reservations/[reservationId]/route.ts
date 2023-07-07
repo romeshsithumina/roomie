f10f05ff4562872447cce1d37dcf411ae5d4e881
Romesh Senarathna <romeshsithumina@gmail.com>
2023-07-07 23:56:26 +0530

    Completed My reservations

---
+++ app/api/reservations/[reservationId]/route.ts
@@ -0,0 +1,39 @@
+import { NextResponse } from "next/server";
+
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import prisma from "@/app/libs/prismadb";
+
+interface IParams {
+  reservationId?: string;
+}
+
+export async function DELETE(
+  request: Request,
+  { params }: { params: IParams }
+) {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return NextResponse.error();
+  }
+
+  const { reservationId } = params;
+
+  if (!reservationId || typeof reservationId !== "string") {
+    throw new Error("Invalid ID");
+  }
+
+  const reservation = await prisma.reservation.deleteMany({
+    where: {
+      id: reservationId,
+      OR: [
+        // allowing only the user who made the reservation
+        // or property owner can delete the reservation
+        { userId: currentUser.id },
+        { listing: { userId: currentUser.id } },
+      ],
+    },
+  });
+
+  return NextResponse.json(reservation);
+}
